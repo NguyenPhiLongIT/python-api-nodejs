961d16e433937882ba68bdb36bb2e3dfeddaf0db
Phi Long <nguyenphilong.it.123@gmail.com>
2024-04-29 12:30:52 +0700

    add view

--- python/app.py
+++ python/app.py
@@ -4,6 +4,7 @@ import json
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 import base64
+import cv2 as cv
 
 app = Flask(__name__)
 CORS(app)
@@ -11,17 +12,16 @@ app = flask.Flask(__name__, template_folder="templates")
 
 @app.route('/pyserver', methods=['POST', 'GET'])
 def call_python():
-    import cv2 # type: ignore
-    im = cv2.imread('image/CoinsA.png')
+    
+    im = cv.imread('image/CoinsA.png')
 
     return render_template("index.ejs", imageSrc="image/CoinsA.png")
 
 @app.route('/gallery', methods=['POST', 'GET'])
 def get_gallery():
-    import cv2 # type: ignore
     im_names = []
-    im = cv2.imread('../public/image/CoinsA.png')
-    ret1, jpeg1 = cv2.imencode('.png', im)
+    im = cv.imread('../public/image/CoinsA.png')
+    ret1, jpeg1 = cv.imencode('.png', im)
     print(im_names)
     im_names.append(jpeg1.tobytes())
     return render_template("gallery.html", image_names=im_names)
